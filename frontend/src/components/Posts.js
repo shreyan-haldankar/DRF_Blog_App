64d69fcbf223e7d7c4b8dd4716078459d85ace32
Shreyan <shreyanhaldankar@gmail.com>
2022-12-05 02:36:15 +0530

    Individual Post Linking with API

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -6,6 +6,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
   cardMedia: {
@@ -47,11 +48,16 @@ const Posts = (props) => {
               // Enterprise card is full width at sm breakpoint
               <Grid item key={post.id} xs={12} md={4}>
                 <Card className={classes.card}>
+                <Link 
+                color="textPrimary"
+                href={'post/' + post.slug}
+                className={classes.link}>
                   <CardMedia
                     className={classes.cardMedia}
                     image="https://source.unsplash.com/random"
                     title="Image title"
                   />
+                  </Link>
                   <CardContent className={classes.cardContent}>
                     <Typography
                       gutterBottom
