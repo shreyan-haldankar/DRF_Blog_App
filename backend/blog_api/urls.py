1a0c0f1750c23338deeffd8e7edb636137696d89
Shreyan <shreyanhaldankar@gmail.com>
2022-12-14 02:20:48 +0530

    Added search functionality and filters

--- backend/blog_api/urls.py
+++ backend/blog_api/urls.py
@@ -1,8 +1,19 @@
 from .views import PostList
 from rest_framework.routers import DefaultRouter
+from .views import PostList, PostDetail, PostListDetailFilter
+from django.urls import path
+
 
 app_name = 'blog_api'
-router = DefaultRouter()
-router.register('', PostList, basename='post')
+# router = DefaultRouter()
+# router.register('', PostList, basename='post')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+
+
+urlpatterns = [
+    path('posts/', PostDetail.as_view(), name='detailcreate'),
+    path('search/', PostListDetailFilter.as_view(), name='postsearch'),
+    path('', PostList.as_view(), name='listcreate'),
+]
--- backend/blog_api/views.py
+++ backend/blog_api/views.py
@@ -2,9 +2,10 @@ from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
 from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions, IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+# from rest_framework import 
 # Create your views here.
 
 
@@ -22,18 +23,18 @@ class PostUserWritePermission(BasePermission):
 
 
 # Model Viewsets
-class PostList(viewsets.ModelViewSet):
-    permission_classes = [PostUserWritePermission]
-    serializer_class = PostSerializer
+# class PostList(viewsets.ModelViewSet):
+#     permission_classes = [PostUserWritePermission]
+#     serializer_class = PostSerializer
 
-    # Custom get object
-    def get_object(self, queryset=None, **kwargs):
-        item = self.kwargs.get('pk')
-        return get_object_or_404(Post, slug=item)
+#     # Custom get object
+#     def get_object(self, queryset=None, **kwargs):
+#         item = self.kwargs.get('pk')
+#         return get_object_or_404(Post, slug=item)
 
-    # Define a custom query set
-    def get_queryset(self):
-        return Post.objects.all()
+#     # Define a custom query set
+#     def get_queryset(self):
+#         return Post.objects.all()
 
 
 # class PostList(viewsets.ViewSet):
@@ -51,15 +52,40 @@ class PostList(viewsets.ModelViewSet):
 
 
 
-# class PostListView(generics.ListCreateAPIView):
-#     # Getting all the Posts that are published
-#     permission_classes = [IsAuthenticated]
-#     queryset = Post.postobjects.all()
-#     serializer_class = PostSerializer
+class PostList(generics.ListCreateAPIView):
+    # Getting all the Posts that are published
+    permission_classes = [IsAuthenticated]
+    # queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Post.objects.filter(author=user)
+        return queryset
 
+class PostDetail(generics.ListAPIView):
+    # permission_classes = [PostUserWritePermission]
+    # queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-# class PostDetailView(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
-#     permission_classes = [PostUserWritePermission]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+    # def get_queryset(self):
+    #     slug = self.kwargs['pk']
+    #     queryset = Post.objects.filter(id=slug)
+    #     return queryset
+
+    # To get query params we have request.query_params.get
+    def get_queryset(self):
+        slug = self.request.query_params.get("slug", None)
+        return Post.objects.filter(slug=slug)
+
+class PostListDetailFilter(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    # Search filter methods
+    # '^' Starts-with search.
+    # '=' Exact matches.
+    # '@' Full-text search. (Currently only supported Django's PostgreSQL backend.)
+    # '$' Regex search.
+    filter_backend = [filters.SearchFilter]
+    search_fields = ['^slug']
